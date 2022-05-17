47fbdeb2da1dffc555b731f58eb1a56c7a22ce7c
bayo <adebayoadeyeye224@gmail.com>
2022-05-17 14:25:27 -0700

    doing my card project by adding javascript

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+$("#nin").submit(function (event){
+    event.preventDefault();
+    let deck =[];
+    let suits = ["diamonds", "spades", "hearts", "clubs"];
+    let values =["Ace", "2","3","4","5","6","7","8","9","10","jack","queen","king"];
+
+    suits.forEach(function(suit) {
+        values.forEach(function(value) {
+            deck.push(`${value}  ${suit}`);
+        });
+    });
+
+    deck.forEach(function(card){
+        $("#output").append("<li>".concat (card,"<li>"));
+
+    })
+
+});
+
+
+});
